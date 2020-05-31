d935c97a2f86636442875a48dface86e6a13b342
Whitchurch Muthumani <neoblackcyptron@gmail.com>
2020-05-31 13:22:14 -0700

    ppush

--- practice/App.xaml.cs
+++ practice/App.xaml.cs
@@ -1,14 +1,23 @@
 ﻿using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using SQLite;
+using System.IO;
 
 namespace practice
 {
     public partial class App : Application
     {
+        public static string dbpath;
+        readonly SQLiteAsyncConnection conn;
+
         public App()
         {
             InitializeComponent();
+            dbpath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "PracticeDatabase.db3");
+            conn = new SQLiteAsyncConnection(dbpath);
+
+            conn.CreateTableAsync<Models.model_gunownership>().Wait();
 
             MainPage = new MainPage();
         }
